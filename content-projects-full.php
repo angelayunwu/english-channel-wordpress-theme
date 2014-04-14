52dcfbc6f0541b5a4c09e2609746cc039e2c6d76
Laura Henze <laurahenze@gmail.com>
2014-04-14 14:01:00 -0400

    Adding Working groups meta to project page

--- content-projects-full.php
+++ content-projects-full.php
@@ -1,5 +1,5 @@
 <?php
-/**
+/**  Template for individual project page
  */
 ?>
 
@@ -12,19 +12,19 @@
 
 		<?php the_content(); ?>
 		<div class="all-meta">
-			<?php $ptype_list = wp_get_object_terms($post->ID, 'project-types');
-		if(!empty($ptype_list)){
-  			if(!is_wp_error( $ptype_list )){
-  				$numItems = count($ptype_list);
-				$i = 0;
-				echo '<div class="project-kind">Kind: ';
-				foreach($ptype_list as $p){
-		  			echo '<span><a href="' . get_term_link($p->slug, 'project-types') . '">' . $p->name .'</a></span>'; 
-		  				if (++$i != $numItems) {echo ', ';}
-				}
-				echo '</div>';
-  			}	
-		}
+		<?php $ptype_list = wp_get_object_terms($post->ID, 'project-types');
+			if(!empty($ptype_list)){
+				if(!is_wp_error( $ptype_list )){
+					$numItems = count($ptype_list);
+					$i = 0;
+					echo '<div class="project-kind">Kind: ';
+					foreach($ptype_list as $p){
+			  			echo '<span><a href="' . get_term_link($p->slug, 'project-types') . '">' . $p->name .'</a></span>'; 
+			  			if (++$i != $numItems) {echo ', ';}
+					}
+					echo '</div>';
+				}	
+			}
 		?>
 		<?php the_tags('<div class="meta-tags">Tags: ', ', ', '</div>'); ?> 
 
@@ -39,10 +39,22 @@
 		if (!empty($s)) {
 			echo "<div class='meta-status'>Status: " . $s . "</div>"; 
 		}
-
-		
-
-
+		$wg_list = wp_get_object_terms($post->ID, 'working-groups');
+			if(!empty($wg_list)){
+				if(!is_wp_error( $wg_list )){
+					$numItems = count($wg_list);
+					$i = 0;
+					echo '<div class="project-kind">Working Group: ';
+					foreach($wg_list as $w){
+						$uu =  get_term_link($w->slug, 'working-groups');
+						echo '<span><a href="' . $uu . '">' . $w->name .'</a></span>'; 
+			
+			  			if (++$i != $numItems) {echo ', ';}
+					}
+					echo '</div>';
+				}	
+			}
+	
 
 ?>
 	
